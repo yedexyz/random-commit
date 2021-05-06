574b1018dc108df16af948246b9470f57f524da8
Dmitry Porunov <hi@dmitry.email>
2021-05-06 22:58:18 +0300

    I think now it works

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::fs;
+use serde::{Deserialize, Serialize};
+use rand::seq::SliceRandom;
+
+#[derive(Serialize, Deserialize)]
+struct Commits {
+    messages: Vec<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let data = fs::read_to_string("commits.json")
+        .expect("Failed to read commits.json");
+
+    let commits: Commits = serde_json::from_str(&data)
+        .expect("Failed to parse commits.json");
+
+    let random_commit = commits.messages.choose(&mut rand::thread_rng())
+        .expect("Failed to pick random commit message");
+
+    println!("{}", random_commit);
 }
