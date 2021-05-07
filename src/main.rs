b7f30fbd659d1e829e986e45122e0bf904a86ab7
Dmitry Porunov <hi@dmitry.email>
2021-05-07 11:33:11 +0300

    Fix what was broken

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::sync::Mutex;
 
-use actix_web::{App, get, HttpResponse, HttpServer, web};
+use actix_web::{App, get, HttpResponse, HttpServer, web, middleware};
 use rand::seq::SliceRandom;
 
 mod commits;
@@ -10,19 +10,29 @@ struct Commits {
 }
 
 #[get("/")]
-async fn index(data: web::Data<Mutex<Commits>>) -> HttpResponse {
+async fn get_random_commit(data: web::Data<Mutex<Commits>>) -> HttpResponse {
     let data = data.lock().unwrap();
     HttpResponse::Ok().body(format!("{}\n", data.messages.choose(&mut rand::thread_rng()).unwrap()))
 }
 
+#[get("/health")]
+async fn health() -> HttpResponse {
+    HttpResponse::Ok().body("Healthy\n")
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "actix_web=info,actix_server=info");
+    env_logger::init();
+
     let commits= web::Data::new(Mutex::new(Commits { messages: commits::get_commits() }));
 
     HttpServer::new(move || {
         App::new()
+            .wrap(middleware::Logger::default())
             .app_data(commits.clone())
-            .service(index)
+            .service(get_random_commit)
+            .service(health)
     })
         .bind("0.0.0.0:8080")?
         .run()
