cffb9eafbbd4ce7e170cd2db367e4cab1dec9152
Dmitry Porunov <hi@dmitry.email>
2021-05-07 10:38:47 +0300

    Fix it for real

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::sync::Mutex;
 
-use actix_web::{App, get, HttpResponse, HttpServer, web, middleware::Logger};
+use actix_web::{App, get, HttpResponse, HttpServer, web};
 use rand::seq::SliceRandom;
 
 mod commits;
@@ -19,15 +19,12 @@ async fn index(data: web::Data<Mutex<Commits>>) -> HttpResponse {
 async fn main() -> std::io::Result<()> {
     let commits= web::Data::new(Mutex::new(Commits { messages: commits::get_commits() }));
 
-    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
-
     HttpServer::new(move || {
         App::new()
-            .wrap(Logger::new("Status: %s"))
             .app_data(commits.clone())
             .service(index)
     })
-        .bind("0.0.0.0:5000")?
+        .bind("0.0.0.0:8080")?
         .run()
         .await
 }
