0bb6d84f80fae557c05793065223f0255f8e887a
Dmitry Porunov <hi@dmitry.email>
2021-05-07 10:00:25 +0300

    Super long commit message goes here, something like 100 words and lots of characters! Woohoo!

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,34 @@
 use std::fs;
-use serde::{Deserialize, Serialize};
+use std::sync::Mutex;
+
+use actix_web::{App, get, HttpResponse, HttpServer, web};
 use rand::seq::SliceRandom;
+use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
 struct Commits {
     messages: Vec<String>,
 }
 
-fn main() {
-    let data = fs::read_to_string("commits.json")
-        .expect("Failed to read commits.json");
+#[get("/")]
+async fn index(data: web::Data<Mutex<Commits>>) -> HttpResponse {
+    let data = data.lock().unwrap();
+    HttpResponse::Ok().body(format!("{}\n", data.messages.choose(&mut rand::thread_rng()).unwrap()))
+}
 
-    let commits: Commits = serde_json::from_str(&data)
-        .expect("Failed to parse commits.json");
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("Starting actix-web server");
+    let data: String = fs::read_to_string("commits.json").expect("Failed to read commits.json");
+    let commits: Commits = serde_json::from_str(&data).expect("Failed to parse commits.json");
+    let commit_messages = web::Data::new(Mutex::new(Commits { messages: commits.messages }));
 
-    let random_commit = commits.messages.choose(&mut rand::thread_rng())
-        .expect("Failed to pick random commit message");
-
-    println!("{}", random_commit);
+    HttpServer::new(move || {
+        App::new()
+            .app_data(commit_messages.clone())
+            .service(index)
+    })
+        .bind("0.0.0.0:5000")?
+        .run()
+        .await
 }
