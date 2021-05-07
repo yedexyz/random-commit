4aa34b26a11872f9f4f5906751aa3df5ec9fe7d7
Dmitry Porunov <hi@dmitry.email>
2021-05-07 18:57:05 +0300

    Checkout completely set up, I hope this still works lord

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use std::sync::Mutex;
 
-use actix_web::{App, get, HttpResponse, HttpServer, web, middleware};
+use actix_web::{App, get, HttpResponse, HttpServer, web, middleware, HttpRequest};
 use rand::seq::SliceRandom;
+use askama::Template;
+use actix_web::http::{HeaderValue, header};
+use actix_web::body::Body;
+use actix_web::http::header::ToStrError;
 
 mod commits;
 
@@ -9,10 +13,71 @@ struct Commits {
     messages: Vec<&'static str>,
 }
 
+#[derive(Template)]
+#[template(path = "dist/index.html")]
+struct CommitTemplate<'a> {
+    commit: &'a str,
+}
+
+fn get_user_agent<'a>(req: &'a HttpRequest) -> Option<&'a str> {
+    req.headers().get("User-Agent")?.to_str().ok()
+}
+
 #[get("/")]
-async fn get_random_commit(data: web::Data<Mutex<Commits>>) -> HttpResponse {
+async fn get_random_commit(data: web::Data<Mutex<Commits>>, req: HttpRequest) -> HttpResponse<Body> {
     let data = data.lock().unwrap();
-    HttpResponse::Ok().body(format!("{}\n", data.messages.choose(&mut rand::thread_rng()).unwrap()))
+
+    let user_agent = req
+        .headers()
+        .get(header::USER_AGENT)
+        .map(|hdr| hdr.to_str());
+
+    match user_agent {
+        Some(Ok(user_agent)) => {
+            if user_agent.contains("curl") {
+                HttpResponse::Ok().content_type("text/plain").body(format!("{}\n", data.messages.choose(&mut rand::thread_rng()).unwrap()))
+            } else {
+                let s = CommitTemplate {
+                    commit: data.messages.choose(&mut rand::thread_rng()).unwrap()
+                }
+                    .render()
+                    .unwrap();
+
+                CommitTemplate {
+                    commit: data.messages.choose(&mut rand::thread_rng()).unwrap()
+                };
+
+                HttpResponse::Ok().content_type("text/html").body(s)
+            }
+        }
+        Some(Err(_)) => {
+            let s = CommitTemplate {
+                commit: data.messages.choose(&mut rand::thread_rng()).unwrap()
+            }
+                .render()
+                .unwrap();
+
+            CommitTemplate {
+                commit: data.messages.choose(&mut rand::thread_rng()).unwrap()
+            };
+
+            HttpResponse::Ok().content_type("text/html").body(s)
+
+        }
+        None => {
+            let s = CommitTemplate {
+                commit: data.messages.choose(&mut rand::thread_rng()).unwrap()
+            }
+                .render()
+                .unwrap();
+
+            CommitTemplate {
+                commit: data.messages.choose(&mut rand::thread_rng()).unwrap()
+            };
+
+            HttpResponse::Ok().content_type("text/html").body(s)
+        }
+    }
 }
 
 #[get("/health")]
@@ -25,7 +90,7 @@ async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "actix_web=info,actix_server=info");
     env_logger::init();
 
-    let commits= web::Data::new(Mutex::new(Commits { messages: commits::get_commits() }));
+    let commits = web::Data::new(Mutex::new(Commits { messages: commits::get_commits() }));
 
     HttpServer::new(move || {
         App::new()
